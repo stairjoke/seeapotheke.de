46b01b7aec77cea7f31835c861daa1f21c7ac653
stairjoke <wenzel@wenzels.design>
2024-05-25 14:45:29 +0200

    enabled @auto for styles

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -5,6 +5,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?= css([
-			'assets/css/global.css'
+			'assets/css/global.css',
+			'@auto' //https://getkirby.com/docs/reference/templates/helpers/css
 		]) ?>
 	</head>
