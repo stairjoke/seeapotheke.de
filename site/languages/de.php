7bf4ab6dc46a7ca10d56c74cccfa58e4d699f004
stairjoke <wenzel@wenzels.design>
2024-05-25 14:44:21 +0200

    enabled multi-language support

---
+++ site/languages/de.php
@@ -0,0 +1,15 @@
+<?php
+return [
+	'code' => 'de',
+	'default' => true,
+	'direction' => 'ltr',
+	'locale' => [
+		'LC_ALL' => 'de_DE.UTF-8'
+	],
+	'name' => 'Deutsch',
+	'url' => '/',
+	'translations' => [
+		'helloWorld' => "Hallo Welt!"
+	]
+];
+?>
--- site/snippets/head.php
+++ site/snippets/head.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="de">
 	<head>
-		<title></title>
+		<title><?= $site->title() ?> &gt; <?= $page->title() ?></title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="css/style.css" rel="stylesheet">
+		<?= css([
+			'assets/css/global.css'
+		]) ?>
 	</head>
