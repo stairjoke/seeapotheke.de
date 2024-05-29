7f1730cab9b3460c51f95d24e5393b052905e442
stairjoke <wenzel@wenzels.design>
2024-05-29 20:09:29 +0200

    added translations for days of the week in German

--- site/languages/de.php
+++ site/languages/de.php
@@ -1,15 +1,22 @@
 <?php
+
 return [
-	'code' => 'de',
-	'default' => true,
-	'direction' => 'ltr',
-	'locale' => [
-		'LC_ALL' => 'de_DE.UTF-8'
-	],
-	'name' => 'Deutsch',
-	'url' => '/',
-	'translations' => [
-		'openingHours' => "Öffnungszeiten"
-	]
+    'code' => 'de',
+    'default' => true,
+    'direction' => 'ltr',
+    'locale' => [
+        'LC_ALL' => 'de_DE.UTF-8'
+    ],
+    'name' => 'Deutsch',
+    'translations' => [
+        'openingHours' => 'Öffnungszeiten',
+        'mon' => 'Mo',
+        'tue' => 'Di',
+        'wed' => 'Mi',
+        'thu' => 'Do',
+        'fri' => 'Fr',
+        'sat' => 'Sa',
+        'sun' => 'So'
+    ],
+    'url' => '/'
 ];
-?>
