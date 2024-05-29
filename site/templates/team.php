37f062883e9a1f8ac99421e740c3e38a3653c06c
stairjoke <wenzel@wenzels.design>
2024-05-29 15:10:18 +0200

    team—display content and team members

---
+++ site/templates/team.php
@@ -0,0 +1,19 @@
+<?php snippet('head') ?>
+<body>
+	<div class="contain">
+		<?php snippet('header') ?>
+		<main>
+			<?= $page->blocksContent()->toBlocks() ?>
+			<?php
+				if($teamMembers = $page->teamMembers()->toStructure());
+				foreach($teamMembers as $member) : ?>
+					<h3><?= $member->headline() ?></h3>
+					<?= $member->photo()->toFile() ?>
+					<?= $member->text()->kb() ?>
+				<?php endforeach;
+			?>
+		</main>
+	</div>
+	<?php snippet('footer') ?>
+</body>
+<?php snippet('foot') ?>
