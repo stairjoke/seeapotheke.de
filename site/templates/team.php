caedfc2c88e4c3969508cd7f6f6a6857e23c9909
stairjoke <wenzel@wenzels.design>
2024-05-29 15:47:14 +0200

    prettify

--- site/templates/team.php
+++ site/templates/team.php
@@ -6,11 +6,12 @@
 			<?= $page->blocksContent()->toBlocks() ?>
 			<?php
 				if($teamMembers = $page->teamMembers()->toStructure()) :
-				foreach($teamMembers as $member) : ?>
-					<h3><?= $member->headline() ?></h3>
-					<?= $member->photo()->toFile() ?>
-					<?= $member->text()->kb() ?>
-				<?php endforeach; endif;
+					foreach($teamMembers as $member) : ?>
+						<h3><?= $member->headline() ?></h3>
+						<?= $member->photo()->toFile() ?>
+						<?= $member->text()->kb() ?>
+					<?php endforeach;
+				endif;
 			?>
 		</main>
 	</div>
