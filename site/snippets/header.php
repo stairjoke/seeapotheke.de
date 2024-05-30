21ce4640db52f4f7069b1bade3fc6496db10b8b4
stairjoke <wenzel@wenzels.design>
2024-05-30 21:53:00 +0200

    correction—header now uses two sections for easier layouting

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,31 +1,33 @@
 <header>
-	<section class="logo">
+	<section id="logo">
 		<?= $site->logo()->toFile() ?>
 	</section>
-	<section>
-		<h2><?= t('openingHours') ?></h2>
-		<?php snippet('hours') ?>
-	</section>
-	<section>
-		<h3><?= $site->sidebarSectionNavigation() ?></h3>
-		<?php snippet('nav') ?>
-	</section>
-	<section>
-		<h3><?= $site->sidebarSectionEmergencyServices() ?></h3>
-		<?php snippet('emergencyServices') ?>
-	</section>
-	<section>
-		<h3><?= $site->sidebarSectionLegal() ?></h3>
-		<?php
-			if($items = $site->sidebarSectionLegalPages()->toPages()): ?>
-				<nav><ul>
-					<?php foreach($items as $item) : ?>
-						<li>
-							<a href="<?= $item->url() ?>" <?php echo ($item->isOpen()) ? 'class="current"' : ''; ?>><?= $item->title() ?></a>
-						</li>
-					<?php endforeach; ?>
-				</ul></nav>
-			<?php endif;
-		?>
+	<section id="navBody">
+		<article>
+			<h2><?= t('openingHours') ?></h2>
+			<?php snippet('hours') ?>
+		</article>
+		<article>
+			<h3><?= $site->sidebarSectionNavigation() ?></h3>
+			<?php snippet('nav') ?>
+		</article>
+		<article>
+			<h3><?= $site->sidebarSectionEmergencyServices() ?></h3>
+			<?php snippet('emergencyServices') ?>
+		</article>
+		<article>
+			<h3><?= $site->sidebarSectionLegal() ?></h3>
+			<?php
+				if($items = $site->sidebarSectionLegalPages()->toPages()): ?>
+					<nav><ul>
+						<?php foreach($items as $item) : ?>
+							<li>
+								<a href="<?= $item->url() ?>" <?php echo ($item->isOpen()) ? 'class="current"' : ''; ?>><?= $item->title() ?></a>
+							</li>
+						<?php endforeach; ?>
+					</ul></nav>
+				<?php endif;
+			?>
+		</article>
 	</section>
 </header>
