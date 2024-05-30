cea74e7164aa6a245c3a4d8c2349eb0704025e0e
stairjoke <wenzel@wenzels.design>
2024-05-30 15:21:42 +0200

    emergency service snippet displays data now

---
+++ site/snippets/emergencyServices.php
@@ -0,0 +1,16 @@
+<?php
+if($emergencyServices = $site->standbyService()->toStructure()) :
+	foreach($emergencyServices as $service) : ?>
+		<article>
+			<h4><?= $service->name() ?></h4>
+			<p><?= $service->start()->toDate('d.m, H:i') ?> Uhr &ndash; <?= $service->end()->toDate('d.m, H:i') ?> Uhr</p>
+			<p><?= $service->address() ?></p>
+			<?php
+				if($service->fon()->isNotEmpty()){
+					echo "<p>" . $service->fon() . "</p>";
+				}
+			?>
+		</article>
+	<?php endforeach;
+endif;
+?>
