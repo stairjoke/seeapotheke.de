4e215e702a5416b644aacd77728939acf70e37fa
stairjoke <wenzel@wenzels.design>
2024-07-04 20:57:07 +0200

    enabling new block for apps

--- site/config/config.php
+++ site/config/config.php
@@ -14,7 +14,7 @@ return [
 				'label' => 'Weitere Elemente',
 				'type' => 'group',
 				'fieldsets' => [
-					'video', 'line', 'table', 'maps-and-message', 'floating', 'button'
+					'video', 'line', 'table', 'maps-and-message', 'floating', 'button', 'appBlock'
 				]
 			],
 			'advanced' => [
