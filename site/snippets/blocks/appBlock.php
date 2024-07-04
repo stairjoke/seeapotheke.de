b9a265e6e2db71953693a8cf8fbbc72a8d65e99f
stairjoke <wenzel@wenzels.design>
2024-07-04 20:57:18 +0200

    rendering new block for apps

---
+++ site/snippets/blocks/appBlock.php
@@ -0,0 +1,17 @@
+<div class="appBlock">
+	<?php if($appIcon = $block->icon()->toFile()): ?>
+	<img alt="<?= ($appIcon->alt() != "") ? $appIcon->alt() : "App Ikone für " . $block->name() ?>" src="<?= $appIcon->url() ?>" />
+	<?php endif; ?>
+	<?php if($name = $block->name()): ?>
+		<p><?= $name ?></p>
+	<?php endif; ?>
+	<div class="links">
+		<?php if($appStore = $block->appStore()): ?>
+			<a href="<?= $appStore ?>"><img alt="Apple AppStore Ikone" src="/assets/img/Apple AppStore.svg" /></a>
+		<?php endif; ?>
+		<?php if($playStore = $block->playStore()): ?>
+			<a href="<?= $playStore ?>"><img alt="Apple AppStore Ikone" src="/assets/img/Google SVG.svg" /></a>
+		<?php endif; ?>
+	</div>
+	<?= $block->info()->kt() ?>
+</div>
