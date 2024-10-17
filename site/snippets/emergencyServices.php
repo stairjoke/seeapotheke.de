1f55b295ede1a628bbb7cf37403b03ee9c837732
stairjoke <wenzel@wenzels.design>
2024-10-17 10:31:45 +0200

    Kirby Update

--- site/snippets/emergencyServices.php
+++ site/snippets/emergencyServices.php
@@ -1,17 +1,20 @@
 <?php
 if($emergencyServices = $site->standbyService()->toStructure()) :
-	foreach($emergencyServices as $service) : ?>
-		<article>
-			<h4><?= $service->name() ?></h4>
-			<p><?= $service->start()->toDate('d.m, H:i') ?> Uhr &ndash; <?= $service->end()->toDate('d.m, H:i') ?> Uhr</p>
-			<p><?= $service->address() ?></p>
-			<?php
-				if($service->fon()->isNotEmpty()){
-					echo '<p>'. t('tel') . ': <a href="tel:' . $service->fon() . '">' . $service->fon() . '</a></p>';
-				}
-			?>
-		</article>
-	<?php endforeach;
+	foreach($emergencyServices as $service) :
+		//if date-range covers current date time
+		if(true) : ?>
+			<article>
+				<h4><?= $service->name() ?></h4>
+				<p><?= $service->start()->toDate('d.m, H:i') ?> Uhr &ndash; <?= $service->end()->toDate('d.m, H:i') ?> Uhr</p>
+				<p><?= $service->address() ?></p>
+				<?php
+					if($service->fon()->isNotEmpty()){
+						echo '<p>'. t('tel') . ': <a href="tel:' . $service->fon() . '" target="_blank">' . $service->fon() . '</a></p>';
+					}
+				?>
+			</article>
+	<?php endif;
+		endforeach;
 	if($externalServices = $site->standbyServiceLink()->toStructure()) : ?>
 		<nav>
 			<ol>
